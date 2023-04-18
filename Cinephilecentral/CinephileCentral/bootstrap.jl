6eec5c20744b75ba6c4879677d8b2402bde77eb7
Jasmy Elzha Mathew <84497582+Jasmy-Elzha-Mathew-1715@users.noreply.github.com>
2023-04-18 11:50:50 -0400

    Delete Cinephilecentral directory

--- Cinephilecentral/CinephileCentral/bootstrap.jl
+++
@@ -1,5 +0,0 @@
-(pwd() != @__DIR__) && cd(@__DIR__) # allow starting app from bin/ dir
-
-using CinephileCentral
-const UserApp = CinephileCentral
-CinephileCentral.main()
--- Cinephilecentral/CinephileCentral/test/movies_test.jl
+++
@@ -1,8 +0,0 @@
-using Test, SearchLight, Main.UserApp, Main.UserApp.Movies
-
-@testset "Movy unit tests" begin
-
-  ### Your tests here
-  @test 1 == 1
-
-end;
