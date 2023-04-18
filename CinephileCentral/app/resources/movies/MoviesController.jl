295e58af1468830231dddfa8a6187b1eece5548c
Akhil17-1 <123491479+Akhil17-1@users.noreply.github.com>
2023-04-18 16:35:39 -0400

    Update MoviesController.jl

--- CinephileCentral/app/resources/movies/MoviesController.jl
+++ CinephileCentral/app/resources/movies/MoviesController.jl
@@ -2,13 +2,6 @@ module MoviesController
 
 using Genie, Genie.Renderer, Genie.Renderer.Html, SearchLight, Movies, Genie.Renderer.Json
 
-function search_api()
-  movies = find(Movie,
-              SQLWhereExpression("title LIKE ? OR categories LIKE ? OR description LIKE ? OR actors LIKE ?",
-                                  repeat(['%' * params(:search_movies) * '%'], 4)))
-
-  json(Dict("movies" => movies))
-end
 function index()
   html(:movies, :index, movies = rand(Movie))
 end
